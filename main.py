80ae6f93f1f6139221106d82537fc5d50941932e
Maledict <94766634+MaledictYtb@users.noreply.github.com>
2023-12-09 23:37:28 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from kandinsky import *
+
+def draw_game() :
+
+    fill_rect(107, 0, 1, 222, color(0, 0, 0))
+    fill_rect(214, 0, 1, 222, color(0, 0, 0))
+    fill_rect(0, 73, 320, 1, color(0, 0, 0))
+    fill_rect(0, 148, 320, 1, color(0, 0, 0))
+
+
+draw_game()
